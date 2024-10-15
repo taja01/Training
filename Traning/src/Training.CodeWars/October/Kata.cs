39a17d0613fa50367ffc8d985a8e7387135a180f
taja01 <x-tamas.jarvas@wizzair.com>
2024-10-15 23:22:27 +0200

    Tuesday

---
+++ Traning/src/Training.CodeWars/October/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace Training.CodeWars.October
+{
+    public class Kata
+    {
+        public static int Grow(int[] x)
+        {
+            return x.Aggregate((a, b) => a * b);
+        }
+    }
+}
---
+++ Traning/test/Training.CodeWars.UnitTests/KataTests.cs
@@ -0,0 +1,16 @@
+﻿using Training.CodeWars.October;
+
+namespace Training.CodeWars.UnitTests
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(Kata.Grow(new[] { 1, 2, 3 }), Is.EqualTo(6));
+            Assert.That(Kata.Grow(new[] { 4, 1, 1, 1, 4 }), Is.EqualTo(16));
+            Assert.That(Kata.Grow(new[] { 2, 2, 2, 2, 2, 2 }), Is.EqualTo(64));
+        }
+    }
+}
