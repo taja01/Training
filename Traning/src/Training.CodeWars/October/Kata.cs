f36e3719ab794d100bf2070f6b45acbe5e2b6ff1
taja01 <x-tamas.jarvas@wizzair.com>
2024-10-17 23:54:28 +0200

    Thursday

--- Traning/src/Training.CodeWars/October/Kata.cs
+++ Traning/src/Training.CodeWars/October/Kata.cs
@@ -22,5 +22,25 @@
         {
             return num % factor == 0;
         }
+
+        /// <summary>
+        /// https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/csharp
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int Sum(int[] numbers)
+        {
+            return (numbers == null || numbers.Length < 2) ? 0 : numbers.OrderBy(x => x).Skip(1).SkipLast(1).Sum();
+
+
+
+            /* 
+             * TODO: write performance tests
+              return numbers == null || numbers.Length < 2
+                    ? 0
+                    : numbers.Sum() - numbers.Max() - numbers.Min();
+            */
+
+        }
     }
 }
---
+++ Traning/test/Training.CodeWars.UnitTests/SumKataTests.cs
@@ -0,0 +1,27 @@
+﻿using Training.CodeWars.October;
+
+namespace Training.CodeWars.UnitTests
+{
+    [TestFixture]
+    internal class SumKataTests
+    {
+        [Test]
+        public void SumPositives()
+        {
+            Assert.That(Kata.Sum(new[] { 6, 2, 1, 8, 10 }), Is.EqualTo(16));
+        }
+
+        [Test]
+        public void SumNull()
+        {
+            Assert.That(Kata.Sum(null), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SumAlone()
+        {
+            Assert.That(Kata.Sum(new[] { 10 }), Is.EqualTo(0));
+        }
+
+    }
+}
