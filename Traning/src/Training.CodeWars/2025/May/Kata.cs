12c942682e4cbaf213400ba8460f2c2258c31ba5
taja01 <jarvas.tamas@gmail.com>
2025-05-22 23:57:14 +0200

    adding today traning

---
+++ Traning/src/Training.CodeWars/2025/May/Kata.cs
@@ -0,0 +1,39 @@
+﻿namespace Training.CodeWars._2025.May
+{
+    public class Kata
+    {
+        //https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/csharp
+        /*
+         * Input: Array of elements
+         * ["h","o","l","a"]
+         * 
+         * Output: String with comma delimited elements of the array in th same order.
+         * 
+         * "h,o,l,a"
+         * 
+         * Note: if this seems too simple for you try the next level
+         * Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕
+         */
+        public static string PrintArray(object[] array)
+        {
+            var list = new List<string>();
+
+            foreach (var item in array)
+            {
+                if (item is object[] nestedArray)
+                {
+                    foreach (var nestedItem in nestedArray)
+                    {
+                        list.Add(nestedItem.ToString());
+                    }
+                }
+                else
+                {
+                    list.Add(item.ToString());
+                }
+            }
+
+            return string.Join(",", list);
+        }
+    }
+}
---
+++ Traning/test/Training.CodeWars.UnitTests/PrintArrayTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Training.CodeWars.UnitTests;
+
+[TestFixture]
+public class PrintArrayTests
+{
+
+    [Test]
+    public void BasicTests()
+    {
+        var data = new object[] { 2, 4, 5, 2 };
+        Assert.That(_2025.May.Kata.PrintArray(data), Is.EqualTo("2,4,5,2"), "int test failed");
+    }
+}
