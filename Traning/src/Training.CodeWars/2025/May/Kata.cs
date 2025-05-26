058bc1404d5e88f4ed32a5475af72cac5c2f36f8
taja01 <jarvas.tamas@gmail.com>
2025-05-26 09:46:39 +0200

    adding Monday test #1

--- Traning/src/Training.CodeWars/2025/May/Kata.cs
+++ Traning/src/Training.CodeWars/2025/May/Kata.cs
@@ -46,5 +46,28 @@
         {
             return input?.Length > 0 ? [input.Count(x => x > 0), input.Where(x => x < 0).Sum()] : [];
         }
+
+        /*
+         * Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+         * This equation doesn't work when the age <= 14, so if the age <= 14, use this equation instead:
+         * 
+         * min = age - 0.10 * age
+         * max = age + 0.10 * age
+         * You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form "[min]-[max]"
+
+            Examples:
+            age = 27   =>   "20-40"
+            age = 5    =>   "4-5"
+            age = 17   =>   "15-20"
+         */
+        public static string DatingRange(int age)
+        {
+            var d = (double)age;
+            var min = age > 14 ? Math.Floor((d / 2) + 7) : Math.Floor(d - 0.1 * d);
+            var max = age > 14 ? Math.Floor(2 * (d - 7)) : Math.Floor(d + 0.1 * d);
+
+
+            return $"{min}-{max}";
+        }
     }
 }
---
+++ Traning/test/Training.CodeWars.UnitTests/DatingRangeTests.cs
@@ -0,0 +1,17 @@
+﻿using Training.CodeWars._2025.May;
+
+namespace Training.CodeWars.UnitTests;
+
+public class DatingRangeTests
+{
+
+    [Test, Description("Example Test Cases")]
+    public void Test()
+    {
+        Assert.That(Kata.DatingRange(17), Is.EqualTo("15-20"));
+        Assert.That(Kata.DatingRange(40), Is.EqualTo("27-66"));
+        Assert.That(Kata.DatingRange(15), Is.EqualTo("14-16"));
+        Assert.That(Kata.DatingRange(35), Is.EqualTo("24-56"));
+        Assert.That(Kata.DatingRange(10), Is.EqualTo("9-11"));
+    }
+}
