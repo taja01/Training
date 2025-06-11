dae688e9eef51dd94a0cb646c65b3fc539e808a7
taja01 <jarvas.tamas@gmail.com>
2025-06-11 23:07:14 +0200

    race condi

---
+++ Practise/ThreadSafety/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ThreadSafety
+{
+    internal class Program
+    {
+        private static int Counter = 0;
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Increment);
+            Thread t2 = new Thread(Increment);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Final Counter Value: {Counter}");
+        }
+
+        static void Increment()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Counter++; // Race condition occurs here
+            }
+        }
+    }
+}
