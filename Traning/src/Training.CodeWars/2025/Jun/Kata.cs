8b9613b483ea786262ede01b12d0831f8b7de7b8
taja01 <jarvas.tamas@gmail.com>
2025-06-12 23:50:02 +0200

    start again

---
+++ Traning/src/Training.CodeWars/2025/Jun/Kata.cs
@@ -0,0 +1,12 @@
+﻿namespace Training.CodeWars._2025.Jun
+{
+    public class Kata
+    {
+        public static bool ValidateHello(string greetings)
+        {
+            var hello = new[] { "hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc" };
+
+            return hello.Any(word => greetings.Contains(word, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Traning/test/Training.CodeWars.UnitTests/Jun/ValidateHelloTests.cs
@@ -0,0 +1,21 @@
+﻿using Training.CodeWars._2025.Jun;
+
+namespace Training.CodeWars.UnitTests;
+
+public class ValidateHelloTests
+{
+    [Test]
+    public void Test()
+    {
+        Assert.That(Kata.ValidateHello("hello"), Is.True);
+        Assert.That(Kata.ValidateHello("ciao bella!"), Is.True);
+        Assert.That(Kata.ValidateHello("salut"), Is.True);
+        Assert.That(Kata.ValidateHello("hallo, salut"), Is.True);
+        Assert.That(Kata.ValidateHello("hombre! Hola!"), Is.True);
+        Assert.That(Kata.ValidateHello("Hallo, wie geht's dir?"), Is.True);
+        Assert.That(Kata.ValidateHello("AHOJ!"), Is.True);
+        Assert.That(Kata.ValidateHello("czesc"), Is.True);
+        Assert.That(Kata.ValidateHello("Ahoj"), Is.True);
+        Assert.That(Kata.ValidateHello("meh"), Is.False);
+    }
+}
